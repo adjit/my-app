dc68ec923714a67bd5435e1df47557aabc749ca9
adjit <ajtawil@gmail.com>
2018-06-28 22:22:02 -0400

    minor style tweaks
    made some minor styling tweaks and adjusted no spaces in body data

--- src/app.jsx
+++ src/app.jsx
@@ -13,26 +13,26 @@ class App extends Component {
           "id":1,
           "timestamp":JSON.stringify(new Date(2018,4,1,13,45,23)),
           "title":"Day 1 - My First Blog Post",
-          "body":"Hey everyone, my name is Aaron. I'm here creating this" +
-                  "first reactJS website. Thus this is my first post on the" +
+          "body":"Hey everyone, my name is Aaron. I'm here creating this " +
+                  "first reactJS website. Thus this is my first post on the " +
                   "react website called my-app. Hopefully this goes well."
         },
         {
           "id":2,
           "timestamp":JSON.stringify(new Date(2018,4,15,1,23,45)),
           "title":"Day 15 - What happened?",
-          "body":"Well... it's been 15 days in the debugging bunker. The first"+
-                  "signs of daylight are protruding through the lines of code."+
-                  "If you can read this then that must mean I am on the right"+
+          "body":"Well... it's been 15 days in the debugging bunker. The first "+
+                  "signs of daylight are protruding through the lines of code. "+
+                  "If you can read this then that must mean I am on the right "+
                   "track. Hopefully this will be completed in time."
         },
         {
           "id":3,
           "timestamp":JSON.stringify(new Date(2018,4,16,6,12,28)),
           "title":"Day 16 - Things are looking good",
-          "body":"Oh yeah! Everything seems to be in order here. The site is"+
-                  "up and running. Purring like a beaute. Hopefully things will"+
-                  "continue in this direction.\n"+
+          "body":"Oh yeah! Everything seems to be in order here. The site is "+
+                  "up and running. Purring like a beaute. Hopefully things will "+
+                  "continue in this direction. "+
                   "Singing off for now..."
         }
       ]
